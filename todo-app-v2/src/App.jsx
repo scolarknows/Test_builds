9bd565ac0da61d2a07fe3cbe810028465eed975d
Scolarnos <scolarknows@gmail.com>
2026-03-22 21:17:22 +0530

    Test build v2 complete!

--- todo-app-v2/src/App.jsx
+++ todo-app-v2/src/App.jsx
@@ -4,32 +4,48 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTasks] = useState([]);
 
+
   function addTask() {
-    setTasks(prev => [...prev, inputValue]);
+
+    const newTask = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false
+    }
+    setTasks(prev => [...prev, newTask]);
     setInputValue("");
   }
 
-  function deleteTask() {
-
+  function deleteTask(id) {
+    setTasks(prev => prev.filter(task => task.id !== id))
   }
 
-  function toggleTask() {
+  function toggleTask(id) {
+    setTasks(prev =>
+      prev.map(task =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    )
 
   }
-
-
 
   return (
     <div className='app-container'>
       <div className='top-row'>
         <h1>TO-DO List</h1>
-        <input type='text' placeholder='Enter a Task ...' value={inputValue} />
-        <button>Add</button>
+        <input type='text' placeholder='Enter a Task ...' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button onClick={addTask}>Add</button>
       </div>
-      <div className='todo-item'>
-        <p>Dynamic Text Object or Task</p>
-        <button>Delete</button>
-        <button>Toggle</button>
+      <div>
+        {tasks.map((task) => (
+          <div key={task.id} className='todo-item'>
+            <p style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.text}</p>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+            <button onClick={() => toggleTask(task.id)}>Toggle</button>
+          </div>
+        ))}
       </div>
     </div>
   )
